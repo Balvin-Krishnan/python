4c040ea55356129eb6a1a6c9844af57bafba3eb4
Jerin Joseph <jeringeorge@gmail.com>
2017-10-01 01:07:54 -0500

    Initial Version

---
+++ hellopython.py
@@ -0,0 +1,29 @@
+import random
+import sys
+import os
+
+print("Hello World");
+
+name = "Balvin";
+print(name)
+
+#Numbers Strings Lists Tuples Directories
+
+print("5+2 = ", 5+2);
+print("5-2 = ", 5-2);
+print("5**2 = ", 5**2);
+
+print ("1+2-3*2 = ", (1+2-3)*2)
+
+quote = "\"Always Remember you are unique"
+
+multi_line_quote = '''just
+like everyone'''
+
+new_string = quote+multi_line_quote
+
+print("%s %s %s" %("i like quote ", quote, multi_line_quote))
+
+print("\n"*5);
+print("i dont like ", end="")
+
---
+++ pythonconditions.py
@@ -0,0 +1,24 @@
+import random
+import sys
+import os
+
+age = 21
+if age>16 :
+    print("You are old enough to drive")
+else :
+    print("You are not allowed to drive")
+
+if age>21 :
+    print("trucks")
+elif age>=16 :
+    print("cars")
+else :
+    print("no drive")
+
+
+if((age>=1) and (age<=18)) :
+    print("you get a birthday")
+elif((age==21) or (age>=65)):
+        print("yours birthda")
+else:
+    print("youa re apartty anis")
