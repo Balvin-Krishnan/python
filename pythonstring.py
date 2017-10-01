7786ee1919b732a5f833d4ba251ecd1d87d3f96f
Balvin Krishnan <kjbalvin@gmail.com>
2017-10-01 01:20:22 -0500

    Initial Version

---
+++ pythonstring.py
@@ -0,0 +1,20 @@
+import random
+import sys
+import os
+
+long_string = "i'll cath you if you fall - the floor"
+
+#print(long_string[0:4])
+
+#print(long_string[-5:])
+
+#print (long_string[:-5])
+
+#print("%c is my %s letter and my number %d number is %.5f"%('X','fav',1,.14))
+
+print(long_string.capitalize())
+
+print(long_string.find("floor"))
+
+quote_list = long_string.split(" ");
+print(quote_list)
---
+++ pythontuples.py
@@ -0,0 +1,10 @@
+import random
+import sys
+import os
+
+pi_tuples = (3,1,4,1,5,9)
+
+new_tuple = list(pi_tuples)
+new_list = tuple(new_tuple)
+
+print(new_list);
